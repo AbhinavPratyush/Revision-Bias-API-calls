d3bd0dbc06082e2845ef79ad7f26475f4be36c3e
AbhinavPratyush <abhinavpratyush273@gmail.com>
2026-03-31 21:47:15 +0530

    Trying io

--- src/main/java/AnythingForTest/LetsRevise/LetsReviseApplication.java
+++ src/main/java/AnythingForTest/LetsRevise/LetsReviseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LetsReviseApplication {
-
+//social conundrum
 	public static void main(String[] args) {
 		SpringApplication.run(LetsReviseApplication.class, args);
 	}
