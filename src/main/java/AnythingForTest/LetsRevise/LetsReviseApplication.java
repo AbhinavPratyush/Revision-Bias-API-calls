795c2f4af0256da0c3240df689814789c2d9c297
ABHINAV PRATYUSH <abhinavpratyush273@gmail.com>
2026-03-31 21:19:33 +0530

    Committed this project

---
+++ src/main/java/AnythingForTest/LetsRevise/LetsReviseApplication.java
@@ -0,0 +1,13 @@
+package AnythingForTest.LetsRevise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsReviseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsReviseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AnythingForTest/LetsRevise/LetsReviseApplicationTests.java
@@ -0,0 +1,13 @@
+package AnythingForTest.LetsRevise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsReviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
